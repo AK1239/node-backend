7735237c8cd198f223ccf0c267f49b35798d42f9
AK1239 <akilkhatri235@gmail.com>
2024-09-09 12:57:36 +0300

    simple product api

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
+const { products } = require("./data");
 
-app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource not found</h1>");
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find((product) => product.id === Number(productID));
+
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist.");
+  }
+
+  res.json(singleProduct);
 });
 
 app.listen(5000, () => {
