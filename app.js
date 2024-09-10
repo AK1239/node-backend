0b125168c64bbc6a2dd4b8c500c13bce5a0d248f
AK1239 <akilkhatri235@gmail.com>
2024-09-10 19:58:51 +0300

    simple get method

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
+let { people } = require("./data");
 
-app.use([logger, authorize]);
-// app.use("/api", logger); This means all urls starting with api
-// it will be applied to them
-
-app.get("/", (req, res) => {
-  res.send("Home");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("Products");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
 app.listen(5000, () => {
